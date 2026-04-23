632e63ea77f0f8e7c25094f77d74867dd7d7b777
Gajendra Sonar <gajusonar05@gmail.com>
2026-04-23 20:55:22 +0530

    Initial commit- added project

---
+++ src/main/java/com/letsEatTiffin/backend/LetsEatTiffinApplication.java
@@ -0,0 +1,13 @@
+package com.letsEatTiffin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsEatTiffinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsEatTiffinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letsEatTiffin/backend/LetsEatTiffinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsEatTiffin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsEatTiffinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
